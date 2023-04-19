18367fc8bda8d5427e5fc02f8c952e92879e2f6b
aditya-una-brands <aditya.singh@una-brands.com>
2023-04-19 13:34:40 +0530

    Daily turnoverpage is half ready

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import DailyTurnOverPage from './Components/DailyTurnOverPage/DailyTurnOverPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      
+      <DailyTurnOverPage/>
     </div>
   )
 }
